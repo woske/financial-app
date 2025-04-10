73dbe679c5a15b96fcdae4314fa87b9421e22773
Baptiste <bapti.valette@gmail.com>
2025-04-09 20:06:00 -0700

    database

--- budgetproject/settings.py
+++ budgetproject/settings.py
@@ -87,9 +87,10 @@ WSGI_APPLICATION = 'budgetproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3'),
-        conn_max_age=600
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
