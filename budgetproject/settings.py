9197645c4bd58e3d2ea423b091852fb5b99d1942
Baptiste <bapti.valette@gmail.com>
2025-04-09 17:03:47 -0700

    hostt

--- budgetproject/settings.py
+++ budgetproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3qw=i^@_5nw_ct@3hzzb(n!zta%#jy%1vk@9_tyw#n05j$u(ik
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['baptiste.pythonanywhere.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['baptiste.pythonanywhere.com', '127.0.0.1', 'localhost', 'financial-app-1-uh2a.onrender.com']
 
 
 # Application definition
